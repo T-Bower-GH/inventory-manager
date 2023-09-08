9d59c11333a40a524d9b2123773b858142e9ba51
Trevor Bower <142063769+T-Bower-GH@users.noreply.github.com>
2023-09-07 21:23:33 -0400

    Delete Part + Product Info Organizer directory
    Directory deleted to remove redundancies

--- Part + Product Info Organizer/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.software1project {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.trevorBower.inventoryManager to javafx.fxml;
-    exports com.trevorBower.inventoryManager;
-}
